c1c840b3cc1af036fbe1cab0911b3f473020b7ab
Jake McDermott <shakey1992jm@hotmail.co.uk>
2024-08-27 18:22:42 +0100

    feat: complete unicafe task 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,29 +7,31 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [acc, setClicks] = useState([])
+  const [accLength, setLength] = useState(0)
   const [total, setTotalClicks] = useState(0)
-  const [average, setAverage] = useState(0)
 
   const handleGood = () => {
+    setGood(good + 1)
     setClicks(acc.concat(good))
-    const updatedCount = good + 1
-    setGood(updatedCount)
-    setTotalClicks(updatedCount+bad+neutral)
+    setTotalClicks(good+bad+neutral + 1)
+    setLength(acc.length + 1)
   }
 
   const handleBad = () => {
+    setBad(bad + 1)
     setClicks(acc.concat(bad))
-    const updatedCount = bad + 1
-    setBad(updatedCount)
-    setTotalClicks(updatedCount+good+neutral)
+    setTotalClicks(bad+good+neutral + 1)
+    setLength(acc.length + 1)
   }
 
   const handleNeutral = () => {
+    setNeutral(neutral + 1)
     setClicks(acc.concat(neutral))
-    const updatedCount = neutral + 1
-    setNeutral(updatedCount)
-    setTotalClicks(updatedCount+good+bad)
+    setTotalClicks(neutral+good+bad + 1)
+    setLength(acc.length + 1)
   }
+
+  console.log(`good: ${good}\nbad: ${bad}\nneutral: ${neutral}\nacc: ${acc}\nacc-length: ${accLength}`)
 
   return (
     <>
@@ -45,6 +47,8 @@ const App = () => {
         <p>bad {bad}</p>
         <p>neutral {neutral}</p>
         <p>total {total}</p>
+        <p>Average {(good-bad) / accLength}</p>
+        <p>Positive {100*(good / accLength)} %</p>
       </div>
     </>
 
