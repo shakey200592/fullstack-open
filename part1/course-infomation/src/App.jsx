a99a030460b740b3991985db8c9cd7dc7423b073
Jake McDermott <shakey1992jm@hotmail.co.uk>
2024-08-27 04:06:43 +0100

    feat: complete exercise 1.5

--- part1/course-infomation/src/App.jsx
+++ part1/course-infomation/src/App.jsx
@@ -1,57 +1,34 @@
-const Header = (props) => {
-    return (
-        <h1>{props.course}</h1>
-    )
-}
+const App = () => {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+          ]
+        }
 
-const Total = (props) => {
-    return (
-        <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-    )
-}
-
-const Part = (props) => {
-    return (
-    <p>{props.part} {props.exercise}</p>
-    )
-}
-
-const Content = () => {
-    const course = 'Half Stack application development'
-    const part1 = {
-      name: 'Fundamentals of React',
-      exercises: 10
-    }
-    const part2 = {
-      name: 'Using props to pass data',
-      exercises: 7
-    }
-    const part3 = {
-      name: 'State of a component',
-      exercises: 14
+    const totalExercises = () => {
+        return course.parts.reduce((acc, current) => acc + current.exercises, 0)
     }
 
     return (
         <>
-            <Header course={course}></Header>
-            <Part part = {part1.name} exercise = {part1.exercises}></Part>
-            <Part part = {part2.name} exercise = {part2.exercises}></Part>
-            <Part part = {part3.name} exercise = {part3.exercises}></Part>
-            <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}></Total>
+            <h1>{course.name}</h1>
+            <p>{course.parts[0].name} {course.parts[0].exercises}</p>
+            <p>{course.parts[1].name} {course.parts[1].exercises}</p>
+            <p>{course.parts[2].name} {course.parts[2].exercises}</p>
+            <p>Total Exercises: {totalExercises()}</p>
         </>
-
-    )
-
-}
-
-
-
-
-const App = () => {
-  return (
-    <div>
-      <Content></Content>
-    </div>
   )
 }
 
