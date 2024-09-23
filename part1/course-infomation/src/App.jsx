7bc9ada0703027d7bfe164a45f1cd1ff43d755a5
Jake McDermott <shakey1992jm@hotmail.co.uk>
2024-09-23 23:54:05 +0100

    initial commiy

--- part1/course-infomation/src/App.jsx
+++ part1/course-infomation/src/App.jsx
@@ -1,35 +1,27 @@
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-          ]
-        }
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
-    const totalExercises = () => {
-        return course.parts.reduce((acc, current) => acc + current.exercises, 0)
-    }
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  );
+};
 
-    return (
-        <>
-            <h1>{course.name}</h1>
-            <p>{course.parts[0].name} {course.parts[0].exercises}</p>
-            <p>{course.parts[1].name} {course.parts[1].exercises}</p>
-            <p>{course.parts[2].name} {course.parts[2].exercises}</p>
-            <p>Total Exercises: {totalExercises()}</p>
-        </>
-  )
-}
-
-export default App
+export default App;
